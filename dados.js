c17030a46b35a1be735ca36d6dce5c8d1fe88c87
Thimelloa <thi-mello@hotmail.com>
2024-09-10 02:29:33 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,23 +1,20 @@
 let dados = [
-    
-{
-    titulo : "Rayssa Leal",
-    descricao : "Rayssa Leal é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, conquistando a prata nos Jogos Olímpicos de Tóquio.",
-    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
-},
-{
-    titulo: "Rebeca Andrade",
-    descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com sua força e determinação. Nos Jogos Olímpicos de Tóquio, conquistou a medalha de ouro no individual geral, sendo a primeira brasileira a conquistar esse feito.",
-    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
-  },
-  {
-    titulo: "Beatriz Souza",
-    descricao: "Beatriz Souza é uma surfista brasileira que se destaca nas ondas brasileiras. Conhecida por sua técnica e paixão pelo surfe, Beatriz é uma das principais promessas do esporte no país.",
-    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" 
-  },
-  {
-    titulo: "Tatiana Weston-Webb",
-    descricao: "Tatiana Guimarães Weston-Webb é uma surfista profissional brasileira-americana, radicada em Kauai, Havaí. Conquistou a medalha de prata nos Jogos Olímpicos de Tóquio 2020 e é conhecida por sua técnica e paixão pelo surfe.",
-    link: "https://pt.wikipedia.org/wiki/Tatiana_Weston-Webb"
-}
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    }
 ];
